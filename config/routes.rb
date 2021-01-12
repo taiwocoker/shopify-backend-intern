030f56c582ee8d15fda64dd141496fd6045e0eec
taiwocoker <taiwofcoker@gmail.com>
2021-01-12 21:39:42 +0100

    Update image routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :images
+  root to: "images#welcome"
+  resources :images, only: %i[create show index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
