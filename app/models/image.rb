8e2eb6c46f4e8b2f57959f23a87b59ed220fd7d1
taiwocoker <taiwofcoker@gmail.com>
2021-01-12 21:41:59 +0100

    Update Image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,10 @@
 class Image < ApplicationRecord
-end
+  include Rails.application.routes.url_helpers
+  has_one_attached :image
+  validates :name, :description, :image, presence: true
+  def img_url
+    url_for(image)
+  end
+  end
+
+
